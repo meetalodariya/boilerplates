486858d4e392b773738000b60493071265cd9272
meet8866 <meetalodariya45@gmail.com>
2020-06-02 17:05:57 +0530

    node-express added

---
+++ nodejs-express-ts/src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import bodyParser from "body-parser";
+const app = express();
+
+// Module imports
+
+// Configuration
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Routes
+app.use();
+app.use();
+
+// Error Handling
